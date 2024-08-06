74d9514c909dbd798f160ad8aa8417a51a9b4f67
Rojali-Jena <rojalijena258@gmail.com>
2024-08-06 11:38:44 +0530

    Add Project Files

---
+++ src/config/gemini.js
@@ -0,0 +1,33 @@
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai"
+  
+  const MODEL_NAME = "gemini-1.5-flash"
+
+  async function run(prompt) {
+    const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
+    const model = genAI.getGenerativeModel({ model : MODEL_NAME }); 
+    
+    const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+   };
+ 
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response = result.response;
+    console.log(response.text());
+    return response.text();
+  }
+  
+export default run;
